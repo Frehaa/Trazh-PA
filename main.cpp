21ec772efda536189c51616acdb88040f482680e
Frehaa <frehaa@hotmail.dk>
2016-08-02 12:26:50 +0200

    Add program main function

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include "http_requester.h"
+#include "tinyxml2.h"
+
+#include <iostream>
+#include <fstream>
+
+
+int main(void)
+{
+
+	HttpRequester httpRequester;
+	httpRequester.newRequest("https://www.reddit.com/r/anime/new.xml");
+
+	std::string content = httpRequester.getContentData();
+
+	tinyxml2::XMLDocument doc;
+	doc.Parse(content.c_str());
+
+	std::string text = doc.FirstChildElement("entry")->GetText();
+
+	int t = 5;
+	std::cin >> t;
+
+
+	return 0;
+}
