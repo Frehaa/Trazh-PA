194f9929bd46ae771963a23298fbb3be3900e2e8
Frederik Haagensen <frehaa@hotmail.dk>
2016-09-29 20:03:19 +0200

    Remove unused helper functions class

--- helper_functions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-// Shorthand function for string find + replace
-std::string& stringReplace(std::string &str, const std::string &oldText, const std::string &newText);
-
-// Shorthand function for string find + replace over multiple iterations
-std::string& stringReplaceAll(std::string &str, const std::string &oldText, const std::string &newText);
